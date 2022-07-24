90b7a85e911a6643e67e570005f767d38b4ce144
Axely7 <axely7@hotmail.com>
2022-07-24 18:53:35 -0500

    update task endpoint created

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -32,8 +32,12 @@ export const createTask = async (req, res) => {
   });
 };
 
-export const updateTask = (req, res) => {
-  res.send("actualizando tarea");
+export const updateTask = async (req, res) => {
+  const result = await pool.query("UPDATE tasks SET ? WHERE id = ?", [
+    req.body,
+    req.params.id,
+  ]);
+  res.json(result);
 };
 
 export const deleteTask = async (req, res) => {
