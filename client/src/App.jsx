2cee83ed575ee6ea712e5105d51ff6824d5f725b
Axely7 <axely7@hotmail.com>
2022-07-24 22:23:14 -0500

    Creating tasks from front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,18 @@ import TasksPage from "./pages/TasksPage";
 import TaskForm from "./pages/TaskForm";
 import NotFound from "./pages/NotFound";
 
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<TasksPage />} />
-      <Route path="/new" element={<TaskForm />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TasksPage />} />
+        <Route path="/new" element={<TaskForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 };
 
--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { Form, Formik } from "formik";
+import { createTaskRequest } from "../api/tasks.api";
 
 const TaskForm = () => {
-  return <div>TaskForm</div>;
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          title: "",
+          description: "",
+        }}
+        onSubmit={async (values, actions) => {
+          console.log(values);
+          try {
+            const response = await createTaskRequest(values);
+            console.log(response);
+            actions.resetForm();
+          } catch (error) {
+            console.log(error);
+          }
+        }}
+      >
+        {({ handleChange, handleSubmit, values, isSubmitting }) => (
+          <Form onSubmit={handleSubmit}>
+            <label>title</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Write a title"
+              onChange={handleChange}
+              value={values.title}
+            />
+
+            <label>description</label>
+            <textarea
+              name="description"
+              rows="3"
+              placeholder="Write a description"
+              onChange={handleChange}
+              value={values.description}
+            ></textarea>
+
+            <button type="onSubmit" disabled={isSubmitting}>
+              {isSubmitting ? "Saving" : "Save"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
 export default TaskForm;
